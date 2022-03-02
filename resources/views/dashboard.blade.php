a5061e62ebe8173e850a385397a7e2a0098c168a
MALE <mariaalejandraquintanilla6@gmail.com>
2022-03-02 03:00:11 -0400

    Fronted de la app

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="h4 font-weight-bold">
-            {{ __('Dashboard') }}
+        
+
+      <h2 class="h4 font-weight-bold">
+            {{ __('RENFE') }}
+           
         </h2>
-    </x-slot>
+       
+    </x-slot> 
 
     <x-jet-welcome />
+
 </x-app-layout>
